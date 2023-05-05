03da4ba889010f54f8faca1071a2e98ecc0e05c5
obirijah <tobirijah@gmail.com>
2023-05-04 17:35:32 -0700

    jjj

--- src/client/layout/Header.js
+++ src/client/layout/Header.js
@@ -38,3 +38,45 @@ export default function Header() {
     </div>
   );
 }
+
+`@import "../styles//theme.scss";
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: #{scaleXL(1520)};
+  color: $white;
+  position: absolute;
+  top: #{scaleXL(58)};
+  left: #{scaleXL(201)};
+  section {
+    display: flex;
+    gap: #{scaleXL(50)};
+    .link {
+      font-size: #{scaleXL(17)};
+      font-family: $poppins;
+      font-weight: 500;
+      line-height: #{scaleXL(28)};
+      color: $white;
+      text-transform: capitalize;
+      letter-spacing: 6%;
+    }
+  }
+}
+
+.logout {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  color: #14162e;
+  background: $white;
+  font-size: #{scaleXL(20)};
+  font-family: "inter";
+  font-weight: 700;
+  gap: #{scaleXL(10)};
+  text-transform: capitalize;
+  padding: #{scaleXL(5)} #{scaleXL(20)};
+  border-radius: #{scaleXL(6)};
+}
+`;
