e9e75ff5a1e296b003344c41a8be1afaa2bb8d85
obirijah <tobirijah@gmail.com>
2023-07-26 02:23:30 -0700

    h

--- src/clientApp/Aunction/components/Index.jsx
+++ src/clientApp/Aunction/components/Index.jsx
@@ -147,7 +147,7 @@ export const RelatedProductsComponent = ({}) => {
     { width: 1, itemsToShow: 2 },
     { width: 550, itemsToShow: 3 },
     { width: 768, itemsToShow: 4 },
-    { width: 1200, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
   ];
 
   return (
